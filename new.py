ec099feb96e1b641bf38044e8c9111ca3dfd1bc1
Rowanleal <rowanleal08@gmail.com>
2024-06-29 16:41:46 +0200

    first day, some practice;)

---
+++ new.py
@@ -0,0 +1,12 @@
+import numpy as np 
+# import matplotlib.pyplot as plt
+
+# numbers=[1,2,3,4,5]
+# print(numbers)
+
+# string= str("hello everyone")
+# print(string)
+sussy=[]
+for i in range(0,121):
+    sussy.append(i)
+print(sussy)
